dc64b24f7e5553bd4e82992878ca10a35d912b84
jason2525 <ilaganjason@ymail.com>
2015-10-09 18:01:28 -0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,20 @@
+<body background="bg2.jpg">
+<br><br><br><center>
+<table border = 1>
+<tr>
+<td colspan = 4><center><img src='apple1.png' width=50% height=30%>
+<tr>
+<td><center><img src=5c.png width=250 height=200><br><center>iPhone 5c 32GB <br>P25,490<br><a href='summary.php?item=iPhone5c32gb&ucost=25490'> Add to Cart</a>
+<td><center><img src=5ss.png width=250 height=200><br><center>iPhone 5s 16GB(ALL COLORS) <br> P27,000 <br><a href='summary.php?item=iPhone5s16gb&ucost=27000'> Add to Cart</a>
+<td><center><img src=5ss.png width=250 height=200><br><center>iPhone 5s 32GB(ALL COLORS) <br> P29,499<br><a href='summary.php?item=iPhone5s32gb&ucost=29499'> Add to Cart</a>
+<td><center><img src=5ss.png width=250 height=200><br><center>iPhone 5s 64GB(ALL COLORS) <br> P32,500<br><a href='summary.php?item=iPhone5s64gb&ucost=32500'> Add to Cart</a>
+
+<tr>
+<td><center><img src=6.png width=250 height=200><br><center>iPhone 6 16GB <br> P34,800<br><a href='summary.php?item=iPhone616gb&ucost=348000'> Add to Cart</a>
+<td><center><img src=6.png width=250 height=200><br><center>iPhone 6 32GB <br> P37,990<br><a href='summary.php?item=iPhone632gb&ucost=37990'> Add to Cart</a>
+<td><center><img src=6.png width=250 height=200><br><center>iPhone 6 64GB <br> P40,990<br><a href='summary.php?item=iPhone664gb&ucost=40990'> Add to Cart</a>
+<td><center><img src=6.png width=250 height=200><br><center>iPhone 6 128GB <br> P45,980<br><a href='summary.php?item=iPhone6128gb&ucost=8000'> Add to Cart</a>
+
+<tr>
+<td colspan = 4 bgcolor=black><font color=white size=1 align=right>Copyright &copy; Jason Ilagan
+</table>
